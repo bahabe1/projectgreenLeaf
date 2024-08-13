dabfa472e22c9f3862f4ba03577318bd4e1b7dff
tuyishime gilbert <140722421+bahabe1@users.noreply.github.com>
2024-08-13 19:43:35 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,513 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Responsive Bootstrap4 Shop Template, Created by Imran Hossain from https://imransdesign.com/">
+
+	<!-- title -->
+	<title>Greenleaf-</title>
+
+	<!-- favicon -->
+	<link rel="shortcut icon" type="image/png" href="assets/img/logo.jpg">
+	<!-- google font -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
+	<!-- fontawesome -->
+	<link rel="stylesheet" href="assets/css/all.min.css">
+	<!-- bootstrap -->
+	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+	<!-- owl carousel -->
+	<link rel="stylesheet" href="assets/css/owl.carousel.css">
+	<!-- magnific popup -->
+	<link rel="stylesheet" href="assets/css/magnific-popup.css">
+	<!-- animate css -->
+	<link rel="stylesheet" href="assets/css/animate.css">
+	<!-- mean menu css -->
+	<link rel="stylesheet" href="assets/css/meanmenu.min.css">
+	<!-- main style -->
+	<link rel="stylesheet" href="assets/css/main.css">
+	<!-- responsive -->
+	<link rel="stylesheet" href="assets/css/responsive.css">
+
+</head>
+<body>
+	
+	<!--PreLoader-->
+    <div class="loader">
+        <div class="loader-inner">
+            <div class="circle"></div>
+        </div>
+    </div>
+    <!--PreLoader Ends-->
+	
+	<!-- header -->
+	<div class="top-header-area" id="sticker">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-sm-12 text-center">
+					<div class="main-menu-wrap">
+						<!-- logo -->
+						<div class="site-logo">
+							<a href="index.html">
+								<img src="assets/img/logo.JPG" alt="" height="70px">
+							</a>
+						</div>
+						<!-- logo -->
+
+						<!-- menu start -->
+						<nav class="main-menu">
+							<ul>
+								<li class="current-list-item"><a href="#">Home</a>
+									
+								</li>
+								<li><a href="#about">About</a></li>
+								
+								
+								
+								<li><a href="#shop">Shop</a>
+								</li>
+									
+										<li><a href="#">Cart</a></li>
+									
+								
+								<li><a href="#contact">Contact</a></li>
+								<li>
+									<li><a href="login.php">login</a></li>
+								
+								</li>
+								<li>
+									<div class="header-icons">
+										<a class="shopping-cart" href="#"><i class="fas fa-shopping-cart"></i></a>
+										<a class="mobile-hide search-bar-icon" href="#"><i class="fas fa-search"></i></a>
+									</div>
+								</li>
+							</ul>
+						</nav>
+						<a class="mobile-show search-bar-icon" href="#"><i class="fas fa-search"></i></a>
+						<div class="mobile-menu"></div>
+						<!-- menu end -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end header -->
+	
+	<!-- search area -->
+	<div class="search-area">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<span class="close-btn"><i class="fas fa-window-close"></i></span>
+					<div class="search-bar">
+						<div class="search-bar-tablecell">
+							<h3>Search For:</h3>
+							<input type="text" placeholder="Keywords">
+							<button type="submit">Search <i class="fas fa-search"></i></button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end search area -->
+
+	
+	<div class="hero-area hero-bg">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-9 offset-lg-2 text-center">
+					<div class="hero-text">
+						<div class="hero-text-tablecell">
+							<p class="subtitle">Green Leaf</p>
+							<h1>The Solution in Agricurture</h1>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+			</div>
+
+		</div>
+	</div>
+	<section id="about">
+		<div class="product-section mt-150 mb-150">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6">
+						<div class="banner-frame"> <img class="img-fluid" src="assets/img/abt.jpg" alt="" />
+						</div>
+					</div>
+					<div class="col-lg-6">
+						<h2 class="green-text">Green <span>Leaf</span></h2>
+						<p>"Green leaf is campany that provide the sercvice of Agricurture product trading from the local maket it make the 24 hours fo 7 days is easy to use for the fa-user
+							green leaf campany it worked in defferent country like rwanda , congo , burundi ,negaria ,soutafrican etc........
+							".</p>
+						<a class="cart-btn" href="#">Read More</a>
+					</div>
+				</div>
+				<div class="row my-5">
+					<div class="col-sm-6 col-lg-4">
+						<div class="service-block-inner">
+							<h3>We are Trusted</h3>
+							<p>The have the different customers and supplyers for all country. </p>
+						</div>
+					</div>
+					<div class="col-sm-6 col-lg-4">
+						<div class="service-block-inner">
+							<h3>We are Professional</h3>
+							<p>Have the supports company </p>
+						</div>
+					</div>
+					<div class="col-sm-6 col-lg-4">
+						<div class="service-block-inner">
+							<h3>We are Expert</h3>
+							<p>the transport the your good at home </p>
+						</div>
+					</div>
+				</div>
+	</section>
+	
+	<div class="list-section pt-80 pb-80">
+		<div class="container">
+
+			<div class="row">
+				<div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
+					<div class="list-box d-flex align-items-center">
+						<div class="list-icon">
+							<i class="fas fa-shipping-fast"></i>
+						</div>
+						<div class="content">
+							<h3>Free Shipping</h3>
+							<p>When order over money</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
+					<div class="list-box d-flex align-items-center">
+						<div class="list-icon">
+							<i class="fas fa-phone-volume"></i>
+						</div>
+						<div class="content">
+							<h3>24/7 Support</h3>
+							<p>Get support all day</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="list-box d-flex justify-content-start align-items-center">
+						<div class="list-icon">
+							<i class="fas fa-sync"></i>
+						</div>
+						<div class="content">
+							<h3>Refund</h3>
+							<p>Get refund within 3 days!</p>
+						</div>
+					</div>
+				</div>
+	<section id="shop">
+		<div class="product-section mt-150 mb-150">
+			<div class="container">
+	
+				<div class="row">
+					<div class="col-md-12">
+						<div class="product-filters">
+							<h3><center>our products</center></h3>
+							<ul>
+								<li class="active" data-filter="*">All</li>
+								<li data-filter=".strawberry">Strawberry</li>
+								<li data-filter=".berry">Berry</li>
+								<li data-filter=".lemon">Lemon</li>
+								<li data-filter=".sunflower">sunflowers</li>
+								<li data-filter=".onyo">onyo</li>
+								<li data-filter=".maize">maize</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+				<div class="row product-lists">
+					<div class="col-lg-4 col-md-6 text-center">
+						
+					<div class="single-product-item">
+							<div class="product-image">
+								<a href="single-product.html"><img src="assets/img/products/product-img-4.jpg" alt=""></a>
+							</div>
+							<h3>Avocado</h3>
+							<p class="product-price"><span>Per Kg</span> 50$ </p>
+							<a href="#" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 text-center">
+						<div class="single-product-item">
+							<div class="product-image">
+								<a href="single-product.html"><img src="assets/img/products/product-img-5.jpg" alt=""></a>
+							</div>
+							<h3>Green Apple</h3>
+							<p class="product-price"><span>Per Kg</span> 45$ </p>
+							<a href="#" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 text-center strawberry">
+						<div class="single-product-item">
+							<div class="product-image">
+								<a href="single-product.html"><img src="assets/img/products/product-img-6.jpg" alt=""></a>
+							</div>
+							<h3>Strawberry</h3>
+							<p class="product-price"><span>Per Kg</span> 80$ </p>
+							<a href="#" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
+						</div>
+					</div>
+				</div>
+	
+				
+				<div class="row">
+					<div class="col-lg-12 text-center">
+						<div class="pagination-wrap">
+							<ul>
+								<li><a href="#">Prev</a></li>
+								<li><a href="#">1</a></li>
+								<li><a class="active" href="#">2</a></li>
+								<li><a href="#">3</a></li>
+								<li><a href="#">Next</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row my-4">
+                <div class="col-12">
+                    <h2 class="noo-sh-title">Meet Our customers</h2>
+                </div>
+                <div class="col-sm-6 col-lg-3">
+                    <div class="hover-team">
+                        <div class="our-team"> <img src="assets/img/team/team-1.jpg" alt="" style="border-radius: 90%;" width="70%" />
+                            <div class="team-content">
+                                <h3 class="title">ange nole</h3> <span class="post">farmer</span> </div>
+                        </div>
+                        <div class="team-description">
+                            <p>the green leaf it help me for the my bussiness becouse it provide the marketing of may products</p>
+                        </div>
+                        <hr class="my-0"> </div>
+                </div>
+				<div class="col-sm-6 col-lg-3">
+                    <div class="hover-team">
+                        <div class="our-team"> <img src="assets/img/team/tearm-4.png" alt=""  style="border-radius: 90%;" width="70%"/>
+                            <div class="team-content">
+                                <h3 class="title">patrice loseum</h3> <span class="post">customer</span> </div>
+                        </div>
+                        <div class="team-description">
+                            <p>the Green Leaf it help me for my bussiness in transport good and services. </p>
+                        </div>
+                        <hr class="my-0"> </div>
+                </div>
+				<div class="col-sm-6 col-lg-3">
+                    <div class="hover-team">
+                        <div class="our-team"> <img src="assets/img/team/team-3.jpg" alt=""  style="border-radius: 90%;" width="70%" />
+                            <div class="team-content">
+                                <h3 class="title">Williamson</h3> <span class="post">Agricurture keeping</span> </div>
+                        </div>
+                        <div class="team-description">
+                            <p>greenleaf it provide the good services for the farmer. </p>
+                        </div>
+                        <hr class="my-0"> </div>
+                </div>
+                <div class="col-sm-6 col-lg-3">
+                    <div class="hover-team">
+                        <div class="our-team"> <img src="assets/img/team/team-2.jpg" alt=""  style="border-radius: 90%;" width="70%" />
+                            <div class="team-content">
+                                <h3 class="title">Kristiana</h3> <span class="post">farmer</span> </div>
+                           
+                        <div class="team-description">
+                            <p>green leaf it help for reduce the time in buying different crops. </p>
+                        </div>
+                        <hr class="my-0"> </div>
+                </div>
+               
+				</div>
+        </div>
+    </div>
+		</div>
+</section>
+
+			
+	<section class="section-padding" id="contact">
+	
+		<div class="contact-from-section mt-150 mb-150">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 mb-5 mb-lg-0">
+						<div class="form-title">
+							<h2>Have you any question?</h2>
+							</div>
+						 <div id="form_status"></div>
+						<div class="contact-form">
+							<form  id="Greenleaf"  method="POST">
+								<p>
+									<input type="text" placeholder="Name" name="name" id="name">
+									<input type="email" placeholder="Email" name="email" id="email">
+								</p>
+								<p>
+									<input type="tel" placeholder="Phone" name="phone" id="phone">
+									<input type="text" placeholder="Subject" name="subject" id="subject">
+								</p>
+								<p><textarea name="message" id="message" cols="30" rows="10" placeholder="Message"></textarea></p>
+								<input type="hidden" name="token" value="FsWga4&@f6aw" />
+								<p><input type="submit" value="Submit" name="submit"></p>
+							</form>
+							<?php
+							include("conn.php");
+							if(isset($_POST['submit'])){
+								$a=$_POST['name'];
+								$b=$_POST['email'];
+								$c=$_POST['phone'];
+								$d=$_POST['subject'];
+								$e=$_POST['message'];
+								$query=mysqli_query($connect,"INSERT INTO submit(name,email,phone,subject,messages) values('$a','$b','$c','$d','$e')");
+								if($query==TRUE){
+									echo"<script>alert('submit is successfull!!!!')</script>";
+								}else{
+									echo"try again";
+	
+								}
+
+							}
+							
+							?>
+						</div>
+					</div>
+					<div class="col-lg-4">
+						<div class="contact-form-wrap">
+							<div class="contact-form-box">
+								<h4><i class="fas fa-map"></i> Shop Address</h4>
+								<p> Rwanda <br> kigali, myamirambo. <br> rwanda</p>
+							</div>
+							<div class="contact-form-box">
+								<h4><i class="far fa-clock"></i> Shop Hours</h4>
+								<p>MON - FRIDAY: 8 to 9 PM <br> SAT - SUN: 10 to 8 PM </p>
+							</div>
+							<div class="contact-form-box">
+								<h4><i class="fas fa-address-book"></i> Contact</h4>
+								<p>Phone: +250798255716 <br> Email:greeleaf@gmail.com</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	
+	</section>
+	
+
+
+	<!-- footer -->
+	<div class="footer-area">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-6">
+					<div class="footer-box about-widget">
+						<h2 class="widget-title">About us</h2>
+						<p>Green leaf is campany that provide the sercvice of Agricurture product trading from the local maket it make the 24 hours fo 7 days is easy to use for the fa-user
+							</p>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<div class="footer-box get-in-touch">
+						<h2 class="widget-title">location</h2>
+						<ul>
+							<li>myamirambo, kigali, rwanda.</li>
+							<li>greenleaf@gmail.com</li>
+							<li>+250798255716</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<div class="footer-box pages">
+						
+						<ul>
+							<li><a href="index.html">Home</a></li>
+							<li><a href="about.html">About</a></li>
+							<li><a href="services.html">Shop</a></li>
+							<li><a href="#">cart</a></li>
+							<li><a href="contact.html">Contact</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<div class="footer-box subscribe">
+						<h2 class="widget-title">newsletter</h2>
+						<p>Subscribe to our mailing list to get the latest updates.</p>
+						<form action="" method="POST">
+							<input type="email" placeholder="Email" name="email">
+							<button type="submit" name="sub"><i class="fas fa-paper-plane"></i></button>
+						</form>
+						<?php
+						include("conn.php");
+						if(isset($_POST['sub'])){
+							$a=$_POST['email'];
+							$qu=mysqli_query($connect,"INSERT INTO newsletter(email) values('$a')");
+							if($qu==TRUE){
+								echo"<script>alert('successfully')</script>";
+							}else{
+								echo"try";
+							}
+						}
+						?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="copyright">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-12">
+					<p>Copyrights &copy; 2024 - <a href="www.bahabesite.net">tuyishime gilbert</a>.<br>
+					
+					</p>
+				</div>
+				<div class="col-lg-6 text-right col-md-12">
+					<div class="social-icons">
+						<ul>
+							<li><a href="www.facebook.com" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+							<li><a href="www.twitter.com" target="_blank"><i class="fab fa-twitter"></i></a></li>
+							<li><a href="www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a></li>
+							<li><a href="www.linkedin.com" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+							
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end copyright -->
+	
+	<!-- jquery -->
+	<script src="assets/js/jquery-1.11.3.min.js"></script>
+	<!-- bootstrap -->
+	<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+	<!-- count down -->
+	<script src="assets/js/jquery.countdown.js"></script>
+	<!-- isotope -->
+	<script src="assets/js/jquery.isotope-3.0.6.min.js"></script>
+	<!-- waypoints -->
+	<script src="assets/js/waypoints.js"></script>
+	<!-- owl carousel -->
+	<script src="assets/js/owl.carousel.min.js"></script>
+	<!-- magnific popup -->
+	<script src="assets/js/jquery.magnific-popup.min.js"></script>
+	<!-- mean menu -->
+	<script src="assets/js/jquery.meanmenu.min.js"></script>
+	<!-- sticker js -->
+	<script src="assets/js/sticker.js"></script>
+	<!-- main js -->
+	<script src="assets/js/main.js"></script>
+
+</body>
+</html>
